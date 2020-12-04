13047af0c43ed31097904d62f51ea4efab607a6d
Bryan Romero <bryanromerou@gmail.com>
2020-12-03 20:48:43 -0800

    remade gifs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,9 +38,6 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarsExample04">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <NavLink exact to="/" className="nav-link font-weight-bolder">Home <span className="sr-only">(current)</span></NavLink>
-                        </li>
-                        <li className="nav-item">
                             <NavLink exact to="/questions" className="nav-link font-weight-bolder">Questions</NavLink>
                         </li>
                         <li className="nav-item">
